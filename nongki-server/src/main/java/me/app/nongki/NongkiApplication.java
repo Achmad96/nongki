167eac3c417185e7529b32cad6db086788e112f4
Achmad Raihan <totsukamail3278@gmail.com>
2025-06-12 22:22:12 +0700

    feat<init project>: set-up nongki-server using Spring Boot framework

---
+++ nongki-server/src/main/java/me/app/nongki/NongkiApplication.java
@@ -0,0 +1,13 @@
+package me.app.nongki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NongkiApplication.class, args);
+	}
+
+}
