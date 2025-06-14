a33a2af376e579b5864205f5f9f9ecb57ab0b7e2
Deska <deskaaryap@gmail.com>
2025-06-14 22:16:09 +0700

    feat <landing Section>: nambahi section landing

--- nongki-client/src/main.jsx
+++ nongki-client/src/main.jsx
@@ -9,8 +9,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/landing" element={<Landing />} /> 
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Landing />} /> 
+        <Route path="/login" element={<Login />} />
         <Route path="/order" element={<Order />} />
       </Routes>
     </BrowserRouter>
